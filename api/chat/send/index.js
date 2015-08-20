b17e74c30fc2f652ab179297e2eacfc9db81abfc
Alex Zammitt <azammitt@gmail.com>
2015-08-19 23:25:46 -0400

    sending message to sns

--- api/chat/send/index.js
+++ api/chat/send/index.js
@@ -1,21 +1,30 @@
 /**
- * API: Users: Show
+ * API: Chat: Send
  */
 
 // Dependencies
 var MiddlewareIncoming = require('jaws-lib').middleware.Incoming;
 
+var ModelChat = require('jaws-lib').models.Chat;
+
+
 // Function
-exports.handler = function(event, context) {
+exports.handler = function (event, context) {
 
     // Process Incoming Request
-    MiddlewareIncoming.process(event, context, function(event, context) {
+    MiddlewareIncoming.process(event, context, function (event, context) {
 
-        /**
-         * Return
-         */
+        //send chat message
+        ModelChat.send(event.body, function (error) {
 
-        return context.succeed(event.req.user);
+            /**
+             * Return
+             */
+             
+
+            return context.succeed({ success: true, body: event.body, error: error });
+
+        });
 
     });
 };
